57744cb16d00512b88ec82f7a636659f807ddcde
Agnieszka <agula2001@o2.pl>
2019-02-17 19:26:22 +0100

    delete shopping cart & add javascript to header

---
+++ js/theme.js
@@ -0,0 +1,21 @@
+const navbar = () => {
+    let icon = document.querySelector('.icon2');
+    let icon2 = icon.classList.contains("icon--no");
+    let header = document.querySelector(".header");
+    let list = document.querySelector('.nav__list');
+    let icon1 = document.querySelector('.icon1');
+
+    if (icon2 === true) {
+        header.classList.add("header--menu");
+        list.classList.add("nav--menu");
+        icon.classList.remove("icon--no");
+        icon1.classList.add("icon--no");
+    } else {
+        header.classList.remove("header--menu");
+        list.classList.remove("nav--menu");
+        icon.classList.add("icon--no");
+        icon1.classList.remove("icon--no");
+    }
+}
+let navIcon = document.querySelector(".nav__icon");
+navIcon.addEventListener("click", navbar, false);
