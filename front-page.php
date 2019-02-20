c1835145f8f4eb522079a89614c9e8127fe35172
Agnieszka <agula2001@o2.pl>
2019-02-20 18:28:28 +0100

    add plugin & custom fields

--- front-page.php
+++ front-page.php
@@ -16,7 +16,7 @@ get_header();
     <div class="section__header">O MNIE</div>
     <div class="bigBox2">
         <img class="image--omnie" src="<?php echo get_template_directory_uri() ?>/img/Filip.jpg" alt="">
-        <div class="text--omnie">Ręcznie robione z sercem i kotem. Każda kartka wyjątkowa, nie ma dwóch takich samych</div>
+        <div class="text--omnie"><?php echo esc_html(get_post_meta(get_the_ID(), "text", true))?></div>
     </div>
 </div>
 <svg width="100%" viewBox="0 0 2304 299" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 0V84.8513C508.747 341.743 917.117 286.017 1305.32 233.042C1636.3 187.876 1952.63 144.711 2304 299V0H0Z" fill="white"/></svg>
---
+++ inc/custom-fields.php
@@ -0,0 +1,53 @@
+<?php add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
+/**
+ * Define the metabox and field configurations.
+ */
+function cmb2_sample_metaboxes() {
+
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = '_yourprefix_';
+
+	/**
+	 * Initiate the metabox
+	 */
+	$cmb = new_cmb2_box( array(
+		'id'            => 'main_page',
+		'title'         => __( 'Edytor strony głównej', 'cmb2' ),
+		'object_types'  => array( 'page', ), // Post type
+		'context'       => 'normal',
+		'priority'      => 'high',
+		'show_names'    => true, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // Keep the metabox closed by default
+	) );
+
+	$cmb->add_field( array(
+		'name'       => __( 'Text główny', 'cmb2' ),
+		//'desc'       => __( 'field description (optional)', 'cmb2' ),
+		'id'         => 'text',
+		'type'       => 'textarea_small',
+	) );
+
+	$cmb->add_field( array(
+		'name' => __( 'O mnie', 'cmb2' ),
+        'id'   => 'url',
+		'type' => 'textarea_small',
+	) );
+
+	$cmb->add_field( array(
+		'name' => __( 'Adres e-mail', 'cmb2' ),
+		'id'   => 'email',
+		'type' => 'text_email',
+		// 'repeatable' => true,
+    ) );
+    
+    $cmb->add_field( array(
+		'name' => __( 'numer telefonu', 'cmb2' ),
+		'id'   => 'numer',
+		'type' => 'text',
+	) );
+
+	// Add other metaboxes as needed
+
+}
+?>
