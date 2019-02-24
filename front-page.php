ad06c41d46646d917d2ee69a1f68cf88d8d75dbb
Agnieszka <agula2001@o2.pl>
2019-02-24 19:58:48 +0100

    modify button class

--- front-page.php
+++ front-page.php
@@ -9,7 +9,9 @@ get_header();
     <img class="image" src="<?php echo get_template_directory_uri() ?>/img/image.png" alt="">
     <div class="box"><?php echo esc_html(get_post_meta(get_the_ID(), "url", true))?></div>
 </div>
-<div class="btn">Zobacz produkt</div>
+<div class="btn--container">
+    <div class="btn">Zobacz produkt</div>
+</div>
     <svg width="100%" viewBox="0 0 2304 478" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M1083 193.761C332.5 359.198 0 0 0 0V478H2304V448.598C2304 448.598 1833.5 28.3228 1083 193.761Z" fill="white"/></svg>
 <div class="oMnie">
@@ -21,7 +23,7 @@ get_header();
 </div>
 <svg width="100%" viewBox="0 0 2304 299" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 0V84.8513C508.747 341.743 917.117 286.017 1305.32 233.042C1636.3 187.876 1952.63 144.711 2304 299V0H0Z" fill="white"/></svg>
 <div class="produkty">
-<div class="section__header">PRODUKTY</div>
+<div id="produkty" class="section__header">PRODUKTY</div>
 <ul class="produkty__list">
     <li class="produkty__item">WSZYSTKO</li>
     <li class="produkty__item">NOWOŚCI</li>
