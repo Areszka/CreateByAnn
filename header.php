b4e072250572ad44bcb458ded760d4071c5c3030
Agnieszka <agula2001@o2.pl>
2019-03-22 21:10:37 +0100

    modify header

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ if (!defined('ABSPATH')) {
 </head>
 
 <body>
-    <div class="header">
+    <header>
         <div class="logo">
             <img class="logo--image logo--shadow" src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt="">
             <span class="logo--text">CreateByAnn</span>
@@ -24,11 +24,11 @@ if (!defined('ABSPATH')) {
 			<svg class="icon1" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
 			<svg class="icon2 icon--no" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
 		</div>
-		<div class="nav">
-			<ul class="nav__list nonav">
+		<div class="nav nav--no">
+			<ul class="nav__list">
                 <li class="nav__item"><a href="#produkty">PRODUKTY</a></li>
                 <li class="nav__item"><a href="#">O MNIE</a></li>
                 <li class="nav__item"><a href="#">KONTAKT</a></li>
             </ul>
         </div>
-	</div>
+	</header>
--- js/theme.js
+++ js/theme.js
@@ -3,20 +3,29 @@ require('./api');
 const navbar = () => {
     let icon = document.querySelector('.icon2');
     let icon2 = icon.classList.contains("icon--no");
-    let header = document.querySelector(".header");
+    let header = document.querySelector("header");
     let list = document.querySelector('.nav__list');
     let icon1 = document.querySelector('.icon1');
+    let body = document.querySelector("body");
+    let nav = document.querySelector(".nav");
+    let logo = document.querySelector(".logo");
 
     if (icon2 === true) {
+        body.style.overflow = "hidden";
         header.classList.add("header--menu");
         list.classList.add("nav--menu");
         icon.classList.remove("icon--no");
         icon1.classList.add("icon--no");
+        nav.classList.remove("nav--no");
+        logo.style.display = "none";
     } else {
+        body.style.overflow = "visible";
         header.classList.remove("header--menu");
         list.classList.remove("nav--menu");
         icon.classList.add("icon--no");
         icon1.classList.remove("icon--no");
+        nav.classList.add("nav--no");
+        logo.style.display = "flex";
     }
 }
 let navIcon = document.querySelector(".nav__icon");
